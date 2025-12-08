26980342e869a88fc0c641d2b6e5f13a43e179bd
Anitha Raajanthira <ppsplash13@gmail.com>
2025-12-08 22:40:51 +0100

    done with caesar

--- caesar/script.js
+++ caesar/script.js
@@ -7,18 +7,18 @@ if (!plainText || !/^[a-zA-Z ]+$/.test(plainText)) {
 
 const arrAlphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 const text = plainText.toLowerCase().split("");
-let encryptedIndex;
+let encIndex;
 const result = text.map((each) => {
   const currentIndex = arrAlphabet.indexOf(each);
-  if (currentIndex === -1) {
-    return each;
-  } else if (offset >= 0) {
-    encryptedIndex = (currentIndex + offset) % 26;
-  } else if (offset < 0) {
-    encryptedIndex = 26 - ((currentIndex * -offset) % 26);
+  if (currentIndex === -1) return each;
+  if (offset >= 0) {
+    encIndex = (currentIndex + offset) % 26;
+    return arrAlphabet[encIndex];
   }
 
-  const output = arrAlphabet[encryptedIndex];
-  return output;
+  const absOffset = Math.abs(offset) % 26;
+  const effOffset = 26 - absOffset;
+  encIndex = currentIndex + effOffset;
+  return arrAlphabet[encIndex];
 });
 console.log(result.join(""));
