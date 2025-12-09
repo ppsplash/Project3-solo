051ebd57ada5fcf7d540c55a3183e1b7c30b06db
Anitha Raajanthira <ppsplash13@gmail.com>
2025-12-09 09:52:45 +0100

    first logic

--- piglatin/script.js
+++ piglatin/script.js
@@ -1,4 +1,15 @@
 const inputText = process.argv[2];
 const text = inputText.split("");
-console.log(text);
-const result = inputText.array.forEach((alphabet) => {});
+
+function validation(input) {
+  if ("aeiou".includes(input[0])) {
+    const remove = input.shift();
+    input.push(remove);
+    const changedText = input.join("") + "way";
+    const finalText = changedText[0].toUpperCase() + changedText.slice(1);
+    return finalText;
+  }
+}
+
+const output = validation(text);
+console.log(output);
