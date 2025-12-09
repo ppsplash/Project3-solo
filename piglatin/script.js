2782661de035a70026e7f986dd5101d86c27d642
Anitha Raajanthira <ppsplash13@gmail.com>
2025-12-09 14:25:24 +0100

    done

--- piglatin/script.js
+++ piglatin/script.js
@@ -1,11 +1,20 @@
 const inputText = process.argv[2];
-const text = inputText.split("");
+const text = inputText.toLowerCase().split("");
 
 function validation(input) {
   if ("aeiou".includes(input[0])) {
-    const remove = input.shift();
-    input.push(remove);
     const changedText = input.join("") + "way";
+    const finalText = changedText[0].toUpperCase() + changedText.slice(1);
+    return finalText;
+  } else {
+    let rightShift = "";
+    let i = 0;
+    while (i < input.length && !"aeiou".includes(input[i])) {
+      rightShift += input[i];
+      i++;
+    }
+    const remText = input.slice(i).join("");
+    const changedText = remText + rightShift + "ay";
     const finalText = changedText[0].toUpperCase() + changedText.slice(1);
     return finalText;
   }
